50b0a063aa122ce4f69b60b93c09f0e29edd3814
darksnoww <darksnowproo@gmail.com>
2024-06-01 23:49:03 +0200

    Update StatManager.lua

--- StatManager.lua
+++ StatManager.lua
@@ -1,21 +1,31 @@
--- File located in ServerScriptService
+-- Script located in ServerScriptService
 game.Players.PlayerAdded:Connect(function(player)
-    local stats = Instance.new("Folder")
-    stats.Name = "leaderstats"
-    stats.Parent = player
+	local stats = Instance.new("Folder")
+	stats.Name = "leaderstats"
+	stats.Parent = player
 
-    local level = Instance.new("IntValue")
-    level.Name = "Level"
-    level.Value = 1
-    level.Parent = stats
+	local health = Instance.new("IntValue")
+	health.Name = "Health"
+	health.Value = 100 
+	health.Parent = stats
 
-    local experience = Instance.new("IntValue")
-    experience.Name = "Experience"
-    experience.Value = 0
-    experience.Parent = stast
+	local swordSkill = Instance.new("IntValue")
+	swordSkill.Name = "SwordSkill"
+	swordSkill.Value = 0 
+	swordSkill.Parent = stats
 
-    local health = Instance.new("IntValue")
-    health.Name = "Health"
-    health.Value = 100
-    health.Parent = stats
-  end)
+	local strength = Instance.new("IntValue")
+	strength.Name = "Strength"
+	strength.Value = 0 
+	strength.Parent = stats
+
+	local stamina = Instance.new("IntValue")
+	stamina.Name = "Stamina"
+	stamina.Value = 50 
+	stamina.Parent = stats
+
+	local mastery = Instance.new("IntValue")
+	mastery.Name = "Mastery"
+	mastery.Value = 0 
+	mastery.Parent = stats
+end)
