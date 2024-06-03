c3bb0e5e198f2e0e8cd7c907de64129be28daaa9
darksnoww <darksnowproo@gmail.com>
2024-06-03 19:43:00 +0200

    Update SprintFunction.lua

--- SprintFunction.lua
+++ SprintFunction.lua
@@ -1,3 +1,4 @@
+-- Put the LocalScript into StarterPlayer --> StarterPlayerScript
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
