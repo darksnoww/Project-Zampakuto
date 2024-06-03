2af659333aeec62ba0a026b61b3aa6516f83ab31
darksnoww <darksnowproo@gmail.com>
2024-06-03 19:38:38 +0200

    Create ShiftSprintConfiguration.lua

---
+++ ShiftSprintConfiguration.lua
@@ -0,0 +1,12 @@
+local Configuration = {}
+
+Configuration.RunKey = Enum.KeyCode.LeftShift
+
+Configuration.WalkSpeed = 16
+Configuration.RunSpeed = 25
+
+Configuration.WalkFov = 70
+Configuration.RunFov = 80
+Configuration.TweenFovDuration = 0.2 
+
+return Configuration
